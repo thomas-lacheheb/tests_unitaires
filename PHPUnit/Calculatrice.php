ca7fb9ecb0a68e715e5786303131ec5122f03ae2
Lacheheb Thomas <thomas.lacheheb@hotmail.fr>
2021-05-25 16:19:25 +0200

    Création des tests de la calculatrice + docker-compose pour gérer les tests

--- PHPUnit/Calculatrice.php
+++ PHPUnit/Calculatrice.php
@@ -8,6 +8,21 @@ class Calculatrice {
 
     public function add($a, $b) 
     {
-        
+        return $a + $b;
+    }
+
+    public function sub($a, $b) 
+    {
+        return $a - $b;
+    }
+
+    public function mul($a, $b) 
+    {
+        return $a * $b;
+    }
+
+    public function div($a, $b) 
+    {
+        return $a / $b;
     }
 }
