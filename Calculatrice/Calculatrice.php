5f41fd78d853d23a4219172bfd7000e01dfa1e53
Lacheheb Thomas <thomas.lacheheb@hotmail.fr>
2021-05-25 16:52:50 +0200

    Modification du nom du dossier

---
+++ Calculatrice/Calculatrice.php
@@ -0,0 +1,28 @@
+<?php
+
+class Calculatrice {
+    public function __construct()
+    {
+        
+    }
+
+    public function add($a, $b) 
+    {
+        return $a + $b;
+    }
+
+    public function sub($a, $b) 
+    {
+        return $a - $b;
+    }
+
+    public function mul($a, $b) 
+    {
+        return $a * $b;
+    }
+
+    public function div($a, $b) 
+    {
+        return $a / $b;
+    }
+}
