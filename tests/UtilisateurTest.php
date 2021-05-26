f7ad0c336be1e1aea9acd403902e5185e05af6f0
Lacheheb Thomas <thomas.lacheheb@hotmail.fr>
2021-05-26 10:04:18 +0200

    Reformatage du fichier UtilisateurTest

--- tests/UtilisateurTest.php
+++ tests/UtilisateurTest.php
@@ -8,22 +8,22 @@ use PHPUnit\Framework\TestCase;
 
 class UtilisateurTest extends TestCase
 {
-    private $user;
+  private $user;
 
-    /**
-     * Fonction appelé avant les tests, initialise les objets utilisés
-     */
-    public function setUp() : void {
-        $this->user = new Utilisateur("LACHEHEB", "Thomas", "thomas.lacheheb@hotmail.fr", 25);
-    }
+  /**
+   * Fonction appelé avant les tests, initialise les objets utilisés
+   */
+  public function setUp() : void {
+    $this->user = new Utilisateur("LACHEHEB", "Thomas", "thomas.lacheheb@hotmail.fr", 25);
+  }
+  
+  public function tearDown() : void {
+    $this->user = null;
+  }
 
-    public function tearDown() : void {
-		$this->user = null;
-    }
-    
 	public function testIsValidOK() {
-		$this->assertTrue($this->user->isValid());
-	}
+    $this->assertTrue($this->user->isValid());
+  }
 
 	public function testIsValidKO() {
     $this->user->setNom('');
